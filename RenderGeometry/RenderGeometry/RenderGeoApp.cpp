f465b07764dcfe5a0fa2cb56787c5b8318c4a571
regireed89 <regireed89@gmail.com>
2017-09-06 02:31:20 -0500

    .

--- RenderGeometry/RenderGeometry/RenderGeoApp.cpp
+++ RenderGeometry/RenderGeometry/RenderGeoApp.cpp
@@ -113,7 +113,15 @@ void RenderGeoApp::startup()
 	Vertex x37 = { glm::vec4(0, -4, 0, 1), glm::vec4(.1, .1, .1, 1) };
 
 
-	std::vector<Vertex>verts = { x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,x29,x30,x31,x32,x33,x34,x35,x36,x37 };
+	std::vector<Vertex>verts = { 
+							x0,
+						x1,x2,x3,x4,
+				x5,x6,x7,x8,x9,x10,x11,x12,
+		x13,x14,x15,x16,x17,x18,x19,x20,x21,x22,x23,x24,
+				x25,x26,x27,x28,x29,x30,x31,x32,
+					x33,x34,x35,x36,
+							x37 
+	};
 	std::vector<unsigned int>indices = { 0,1,2, 
 		0,2,3,
 		0,3,4,
